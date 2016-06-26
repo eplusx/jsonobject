3a46832f492bcccec2541dc0502efe10275b20f6
Hiroaki Hasegawa <eplusx@gmail.com>
2016-06-26 23:06:31 +0900

    Run doctest also in Python 3.

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,9 +9,12 @@ export PYTHONPATH="${TEST_ROOT}"
 # Abort immediately if any of the commands below fails.
 set -e
 # Run doctest.
-echo "Running doctest..."
-find "${TEST_ROOT}" -name "*.py" | xargs python -m doctest
+echo "Running doctest in Python 2..."
+find "${TEST_ROOT}" -name "*.py" | xargs python2 -m doctest
+echo "Running doctest in Python 3..."
+find "${TEST_ROOT}" -name "*.py" | xargs python3 -m doctest
 # Run py.test tests.
-echo "Running py.test..."
+echo "Running py.test in Python 2..."
 python2 -m pytest "${TEST_ROOT}"
+echo "Running py.test in Python 3..."
 python3 -m pytest "${TEST_ROOT}"
